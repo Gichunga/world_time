33e679050d43ded2d34126653ce3b83d59f44a13
gichunga <gichungasteve6@gmail.com>
2024-06-29 13:33:24 +0300

    fetch todays date from the world time api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -17,16 +17,32 @@ class _LoadingState extends State<Loading> {
     print('initState function ran');
   }
 
-  void getData() async {
-    Response response =
-        await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-    Map data = jsonDecode(response.body);
-    print(data['title']);
+  void fetchTime() async {
+    final response = await http.get(Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Nairobi'));
+    if (response.statusCode == 200) {
+      Map data = jsonDecode(response.body);
+      print(data);
+
+      // get properties from data
+      String datetime = data['datetime'];
+      // the substring of offset from start- inclusive to end- exclusive
+      String offset = data['utc_offset'].substring(1,3);
+      // print(offset);
+
+      // create datetime object
+      DateTime now = DateTime.parse(datetime);
+      // print(now);
+      // parse offset to integer and it today's date
+      now = now.add(Duration(hours: int.parse(offset)));
+      print(now);
+    } else {
+      print('Failed to load data');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
+    fetchTime();
     return const Scaffold(
       body: Text('loading'),
     );
